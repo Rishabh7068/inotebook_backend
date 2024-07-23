f82c4f28afc42725da4e2c08877d919470d4f273
Rishabh Rathore <rishabhrathore@Rishabhs-MacBook-Pro.local>
2024-07-23 10:00:26 +0530

    hash the password and send jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -2,23 +2,55 @@ const express = require("express");
 const router = express.Router();
 const User = require("../model/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-router.post('/',[
-        body('name', 'Enter a valid name').isLength({ min: 3 }),
-        body('email', 'Enter a valid email').isEmail(),
-        body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-    ] , (req, res)=>{ 
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
+const JWT_secret ="Rishabh&inotebook";
+
+router.post(
+  "/creatUser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "Email is already Registered" });
+      }
+
+      const salt = bcrypt.genSaltSync(10);
+      const securepass = await bcrypt.hash(req.body.password, salt);
+
+      user = await User.create({
+        name: req.body.name,
+        password: securepass,
+        email: req.body.email,
+      });
+
+      const data ={
+        user:{
+          id: user.id
         }
-        User.create({
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-          }).then(user => res.json(user))
-          .catch(err=> {console.log(err)
-        res.json({error: 'Please enter a unique value for email', message: err.message})})
-    } )
-    
-    module.exports = router
+      }
+
+      const authtoken = jwt.sign(data,JWT_secret);
+      console.log(authtoken);
+
+      res.json({authtoken});
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Some Error Occurd");
+    }
+  }
+);
+
+module.exports = router;
